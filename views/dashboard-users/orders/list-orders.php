3d4eeac3bd79b2ea0d1a88d71b4cdd5824e305c3
cedricdre <cedric.dreue@gmail.com>
2024-02-18 10:19:20 +0100

    Ajout order bdd

--- views/dashboard-users/orders/list-orders.php
+++ views/dashboard-users/orders/list-orders.php
@@ -25,6 +25,33 @@
                             </tr>
                         </thead>
                         <tbody>
+                            <?php
+                            foreach($orders as $order){ ?>
+                            <tr>
+                                <th scope="row"><?=$order->id_order?></th>
+                                <th><?=$order->created_at?></th>
+                                <th>
+                                    <?php
+                                        if ($product->status == 1) { ?>
+                                            <span class="badge rounded-pill text-bg-warning">En préparation...</span>
+                                    <?php
+                                    } ?>
+                                    <?php
+                                        if ($product->status == 2) { ?>
+                                            <span class="badge rounded-pill text-bg-success"><i class="bi bi-link me-1"></i>À retirer</span>
+                                    <?php
+                                    } ?>
+                                    <?php
+                                        if ($product->status == 3) { ?>
+                                            <span class="badge rounded-pill text-bg-secondary"><i class="bi bi-link me-1"></i>Retiré</span>
+                                    <?php
+                                    } ?>
+                                </th>
+                                <th><?=$order->created_at?></th>
+                                <th><a class="btn btn-outline-success btn-sm" href="/controllers/dashboard-users/products/update-ctrl.php?idproduct=<?=$product->id_product?>" role="button"><i class="bi bi-pencil-fill me-2"></i>Modifier</a></th>
+                            </tr>
+                            <?php
+                            } ?>
                             <tr>
                                 <th scope="row">1</th>
                                 <td>30 janv. 2024</td>
@@ -32,20 +59,7 @@
                                 <td>0,00 <sup>€</sup></td>
                                 <th scope="row"><a class="btn btn-outline-success btn-sm" href="#" role="button">Consulter</a></th>
                             </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>30 janv. 2024</td>
-                                <td><span class="badge rounded-pill text-bg-success">À retirer</span></td>
-                                <td>0,00 <sup>€</sup></td>
-                                <th scope="row"><a class="btn btn-outline-success btn-sm" href="#" role="button">Consulter</a></th>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>30 janv. 2024</td>
-                                <td><span class="badge rounded-pill text-bg-secondary">Retiré</span></td>
-                                <td>0,00 <sup>€</sup></td>
-                                <th scope="row"><a class="btn btn-outline-success btn-sm" href="#" role="button">Consulter</a></th>
-                            </tr>
+
                         </tbody>
                     </table>
                 </div>
